18046ab7733f143d9f767d4730df7dbd782f9458
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 18:25:49 -0800

    Update SimulationController.cs

--- Controlador/SimulationController.cs
+++ Controlador/SimulationController.cs
@@ -1,35 +1,20 @@
 // Controlador/SimulationController.cs
 using MyApp.Modelo;
-using MyApp.Fabrica;  // Importa la clase de la fábrica
-using MyApp.Vista;  // Agrega esta línea si no está presente.
 
 namespace MyApp.Controlador
 {
     public class SimulationController
     {
-        private readonly FabricaRecuerdos _fabricaRecuerdos;
-        private readonly Simulation _simulation;
-
-        public SimulationController()
+        public void StartSimulation()
         {
-            _fabricaRecuerdos = new FabricaRecuerdosConcreta();
-            _simulation = Simulation.Instance;
-        }
-
-        public void IniciarSimulacion()
-        {
-            Console.WriteLine("Introduce el nombre del criminal:");
-            var nombre = Console.ReadLine();
-
-            var tipoCrimen = ConsoleView.GetCrimeTypeFromUser();
-            var criminal = new Criminal { Name = nombre, CrimeType = tipoCrimen };
-
-            var recuerdo = _fabricaRecuerdos.CrearRecuerdo(tipoCrimen);
-            criminal.Memories.Add(recuerdo);
-
-            ConsoleView.ShowMemory(recuerdo);
-
-            _simulation.StartSimulation(criminal);
+            // Asegúrate de pasar un nombre al crear un Criminal
+            var criminal = new Criminal("Juan Pérez")  // Pasamos el nombre aquí
+            {
+                CrimeType = CrimeType.Robo
+            };
+            
+            // Ahora podemos usar 'criminal' para iniciar la simulación
+            Simulation.Instance.StartSimulation(criminal);
         }
     }
 }
