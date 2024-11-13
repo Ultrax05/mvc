6c452147a567d97ad90d05d79ff353a87b1150f5
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 17:36:18 -0800

    Update ConsoleView.cs

--- Vista/ConsoleView.cs
+++ Vista/ConsoleView.cs
@@ -5,16 +5,24 @@ using MyApp.Modelo; // Para usar CrimeType, Memory, etc.
 namespace MyApp.Vista
 {
     public class ConsoleView
+{
+    public static void Run()
     {
-        public static CrimeType GetCrimeTypeFromUser()
-        {
-            Console.WriteLine("Selecciona el tipo de crimen: 1. Robo 2. Agresión 3. Fraude 4. Otro");
-            return Enum.Parse<CrimeType>(Console.ReadLine());
-        }
-
-        public static void ShowMemory(Memory memory)
-        {
-            Console.WriteLine($"Recuerdo: {memory.Content} | Emoción: {memory.Emotion}");
-        }
+        var crimeType = GetCrimeTypeFromUser();
+        var memory = new Memory { Content = "Recuerdo del crimen", Emotion = "Sentimiento" };
+        ShowMemory(memory);
     }
+
+    public static CrimeType GetCrimeTypeFromUser()
+    {
+        Console.WriteLine("Selecciona el tipo de crimen: 1. Robo 2. Agresión 3. Fraude 4. Otro");
+        return Enum.Parse<CrimeType>(Console.ReadLine());
+    }
+
+    public static void ShowMemory(Memory memory)
+    {
+        Console.WriteLine($"Recuerdo: {memory.Content} | Emoción: {memory.Emotion}");
+    }
+}
+
 }
