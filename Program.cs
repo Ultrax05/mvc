7afce181c87e7ae3a2b569326d0d534ac1171091
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 18:36:43 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using MyApp.Vista; // Referencia a la clase Vista
+
 namespace MyApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            ConsoleView.ShowMemory();  // Esto es correcto para un método estático
+            // Crear una instancia de ConsoleView
+            var view = new ConsoleView();
+            
+            // Llamar al método ShowMemory en la instancia
+            view.ShowMemory(); 
         }
     }
 }
