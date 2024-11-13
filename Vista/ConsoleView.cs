05dab59ee92d49faa89bbc5f803d312ff88f9a40
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 16:31:49 -0800

    Create ConsoleView.cs

---
+++ Vista/ConsoleView.cs
@@ -0,0 +1,14 @@
+// Vista/ConsoleView.cs
+public class ConsoleView
+{
+    public static CrimeType GetCrimeTypeFromUser()
+    {
+        Console.WriteLine("Selecciona el tipo de crimen: 1. Robo 2. Agresión 3. Fraude 4. Otro");
+        return Enum.Parse<CrimeType>(Console.ReadLine());
+    }
+
+    public static void ShowMemory(Memory memory)
+    {
+        Console.WriteLine($"Recuerdo: {memory.Content} | Emoción: {memory.Emotion}");
+    }
+}
