dc0cb3809538aa77c59e47dfd0c02b4d6e7214fd
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 16:30:41 -0800

    Create FabricaRecuerdos.cs

---
+++ Fabrica/FabricaRecuerdos.cs
@@ -0,0 +1,18 @@
+// Fabrica/FabricaRecuerdos.cs
+public abstract class FabricaRecuerdos
+{
+    public abstract Memory CrearRecuerdo(CrimeType crimeType);
+}
+
+public class FabricaRecuerdosConcreta : FabricaRecuerdos
+{
+    public override Memory CrearRecuerdo(CrimeType crimeType)
+    {
+        return crimeType switch
+        {
+            CrimeType.Theft => new Memory { Content = "Robaste una tienda", Emotion = "Remordimiento" },
+            CrimeType.Assault => new Memory { Content = "Atacaste a una persona", Emotion = "Arrepentimiento" },
+            _ => new Memory { Content = "Cometiste un crimen", Emotion = "Reflexión" }
+        };
+    }
+}
