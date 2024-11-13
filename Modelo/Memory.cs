d840e622bac8ea4f6c16851792c0191bcc4399bb
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 16:48:34 -0800

    Create Memory.cs

---
+++ Modelo/Memory.cs
@@ -0,0 +1,9 @@
+// Modelo/Memory.cs
+namespace ProyectoCognify.Modelo
+{
+    public class Memory
+    {
+        public string Content { get; set; }
+        public string Emotion { get; set; }
+    }
+}
