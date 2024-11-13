37ce08198ce0de219f39740ff87ca4b89faf1217
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 18:39:39 -0800

    Update FabricaRecuerdos.cs

--- Fabrica/FabricaRecuerdos.cs
+++ Fabrica/FabricaRecuerdos.cs
@@ -16,7 +16,7 @@ namespace MyApp.Fabrica
             {
                 CrimeType.Robo => new Memory { Content = "Robaste una tienda", Emotion = "Remordimiento" },
                 CrimeType.Agresion => new Memory { Content = "Atacaste a una persona", Emotion = "Arrepentimiento" },
-                _ => new Memory { Content = "Cometiste un crimen", Emotion = "Reflexión" }
+                _ => new Memory("Robaste una tienda", "Remordimiento")
             };
         }
     }
