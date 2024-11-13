f9b435a6118b5020e91fe1a6b736d9aa1978a58f
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 16:32:28 -0800

    Create SimulationController.cs

---
+++ Controlador/SimulationController.cs
@@ -0,0 +1,28 @@
+// Controlador/SimulationController.cs
+public class SimulationController
+{
+    private readonly FabricaRecuerdos _fabricaRecuerdos;
+    private readonly Simulation _simulation;
+
+    public SimulationController()
+    {
+        _fabricaRecuerdos = new FabricaRecuerdosConcreta();
+        _simulation = Simulation.Instance;
+    }
+
+    public void IniciarSimulacion()
+    {
+        Console.WriteLine("Introduce el nombre del criminal:");
+        var nombre = Console.ReadLine();
+
+        var tipoCrimen = ConsoleView.GetCrimeTypeFromUser();
+        var criminal = new Criminal { Name = nombre, CrimeType = tipoCrimen };
+
+        var recuerdo = _fabricaRecuerdos.CrearRecuerdo(tipoCrimen);
+        criminal.Memories.Add(recuerdo);
+
+        ConsoleView.ShowMemory(recuerdo);
+
+        _simulation.StartSimulation(criminal);
+    }
+}
