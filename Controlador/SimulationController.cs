b59895fb04e276f09c7b5303b619a769d5593f71
Iván Alvarez <86640993+Ultrax05@users.noreply.github.com>
2024-11-12 16:51:51 -0800

    Update SimulationController.cs

--- Controlador/SimulationController.cs
+++ Controlador/SimulationController.cs
@@ -1,28 +1,36 @@
 // Controlador/SimulationController.cs
-public class SimulationController
+using ProyectoCognify.Modelo;
+using ProyectoCognify.Fabrica;
+using ProyectoCognify.Vista;
+
+namespace ProyectoCognify.Controlador
 {
-    private readonly FabricaRecuerdos _fabricaRecuerdos;
-    private readonly Simulation _simulation;
-
-    public SimulationController()
+    public class SimulationController
     {
-        _fabricaRecuerdos = new FabricaRecuerdosConcreta();
-        _simulation = Simulation.Instance;
-    }
+        private readonly FabricaRecuerdos _fabricaRecuerdos;
+        private readonly Simulation _simulation;
 
-    public void IniciarSimulacion()
-    {
-        Console.WriteLine("Introduce el nombre del criminal:");
-        var nombre = Console.ReadLine();
+        public SimulationController()
+        {
+            _fabricaRecuerdos = new FabricaRecuerdosConcreta();
+            _simulation = Simulation.Instance;
+        }
 
-        var tipoCrimen = ConsoleView.GetCrimeTypeFromUser();
-        var criminal = new Criminal { Name = nombre, CrimeType = tipoCrimen };
+        public void IniciarSimulacion()
+        {
+            Console.WriteLine("Introduce el nombre del criminal:");
+            var nombre = Console.ReadLine();
 
-        var recuerdo = _fabricaRecuerdos.CrearRecuerdo(tipoCrimen);
-        criminal.Memories.Add(recuerdo);
+            var tipoCrimen = ConsoleView.GetCrimeTypeFromUser();
+            var criminal = new Criminal { Name = nombre, CrimeType = tipoCrimen };
 
-        ConsoleView.ShowMemory(recuerdo);
+            var recuerdo = _fabricaRecuerdos.CrearRecuerdo(tipoCrimen);
+            criminal.Memories.Add(recuerdo);
 
-        _simulation.StartSimulation(criminal);
+            ConsoleView.ShowMemory(recuerdo);
+
+            _simulation.StartSimulation(criminal);
+        }
     }
 }
+
